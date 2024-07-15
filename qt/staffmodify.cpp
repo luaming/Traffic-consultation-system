02fff0ec846a41fcbd8936fddd9c59bfe3d48c49
wurldwen <1287356875@qq.com>
2024-07-15 23:04:32 +0800

    车票制作完成

--- qt/staffmodify.cpp
+++ qt/staffmodify.cpp
@@ -6,6 +6,17 @@ StaffModify::StaffModify(QWidget *parent)
     ui->setupUi(this);
     connect(ui->stfbtnCrawler, &QPushButton::clicked, this, &StaffModify::on_stfbtnCrawler_clicked);
     m_pCrawlerWidget = new CrawlerWidget(this);
+    QGridLayout*layout=new QGridLayout();
+    //添加车票begin
+
+    for(int i=0;i<4;i++){
+        Ticket*t=new Ticket();
+        layout->addWidget(t);
+    }//此处添加
+
+    //添加车票end
+    //可以先把票都存入vector arr中再将上面的for循环改为for（auto r:arr)
+    ui->scrollAreaWidgetContents->setLayout(layout);
 }
 
 StaffModify::~StaffModify() {
